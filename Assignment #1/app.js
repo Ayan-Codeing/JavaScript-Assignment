ea602d97c770381fd4f021d50f697f88d5da4220
Ayan-Codeing <ayanadil870@gmail.com>
2026-04-15 18:40:51 +0500

    changed

--- Assignment #1/app.js
+++ Assignment #1/app.js
@@ -1,9 +1,15 @@
 // Q1
-alert("Wellcome")
+// alert("Wellcome")
+
 // Q2
-alert("please enter a valid password");
+// alert("please enter a valid password");
+
 // Q3
-alert("Wellcome to js land... \n Happy coding ")
+// alert("Wellcome to js land... \n Happy coding ")
+
 // Q4 
-alert("wellcome one!")
-alert("wellcome two!")
+// alert("wellcome to JS Land...")
+// alert("Happy Coding")
+
+// Q5
+// console.log("Hello i can run through my web drowser console");

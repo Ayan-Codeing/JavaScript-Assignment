02a11cf1cb704cf3fa53342c865af068429f44f7
Ayan-Codeing <ayanadil870@gmail.com>
2026-04-15 18:09:19 +0500

    first-commit

---
+++ Assignment #1/app.js
@@ -0,0 +1,9 @@
+// Q1
+alert("Wellcome")
+// Q2
+alert("please enter a valid password");
+// Q3
+alert("Wellcome to js land... \n Happy coding ")
+// Q4 
+alert("wellcome one!")
+alert("wellcome two!")
---
+++ Assignment #2/app.js
@@ -0,0 +1,30 @@
+// javasciprt is case sensitive
+
+// data types
+// string , number , bolean , null , undefined , NaN , digiNT
+
+// variables
+// do not use white space in your variable
+// do not start your variables with numbers
+// special charector is not allaowed but this is allowed _  $ 
+
+// let const var
+
+// variables name is idenfitier
+// = is asignment operator
+
+//re dicleration varial repeatly
+// re assigment variable update
+
+var studentName = "Muhammad Ayan" // allowed rediclaration
+let studentPhoneNumber = 2380594045 // re assignment
+let city = 'lahore'
+const isPresend = true // not allowed re assignmenet re dicleration
+
+console.log(studentName);
+console.log(studentPhoneNumber);
+console.log(isPresend);
+console.log(city);
+
+city = "karachi"
+console.log(city);

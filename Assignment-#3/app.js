bcd78d364f6b4bda4e1701da94163cc8c394a47e
Ayan-Codeing <ayanadil870@gmail.com>
2026-04-20 18:32:13 +0500

    done

--- Assignment-#3/app.js
+++ Assignment-#3/app.js
@@ -7,11 +7,14 @@
 // alert("You Have Visit This Site " + visit + " Times")
 
 // Q #3
-// let BirthYear = 2009
-// document.write("my birth years is " +BirthYear+ "\nData Type of my declared ")
+// let birthYear = 2008
+// let type = typeof(birthYear)
+// document.write("My birth years is " +birthYear)
+// document.write(`<br>`)
+// document.write("Data Type of my declared variable is " + type)
 
 // Q #4
-// name = "John Doe"
+// visitorName = "Honey Sings"
 // product = "T-Shirt"
 // Quantity = 5
-// document.write(name + " Ordered " + Quantity+ " " + product+"(s)"+" on xyz_clothing Store")
+// document.write(visitorName + " Ordered " + Quantity+ " " + product+"(s)"+" on xyz_clothing Store")
